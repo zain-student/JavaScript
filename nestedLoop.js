36c6191e2cebfce4c2754d89ee8865c5c0cdbae7
zain-student <zainmalik4989@gmail.com>
2023-10-28 19:03:38 +0500

    nested Loop

---
+++ nestedLoop.js
@@ -0,0 +1,17 @@
+// Loop to Show the Working days of two weeks
+console.log("Week days of two weeks!");
+for (let week = 1; week < 3; week++) {
+  console.log("Week: ", week);
+  for (let day = 1; day < 6; day++) {
+    console.log("Day: ", day);
+  }
+}
+
+// Loop to show the summer months of 3 years
+console.log("Summer Months of three years!");
+for (let year = 2021; year < 2024; year++) {
+  console.log("Year: ", year);
+  for (let month = 6; month < 9; month++) {
+    console.log("Month: ", month);
+  }
+}
--- whileLoop.js
+++ whileLoop.js
@@ -1,6 +1,11 @@
-let i = 10;
-while (i > 0) {
+// let i = 10;
+// while (i > 0) {
+//   console.log(i);
+//   i = i - 1;
+// }
+// console.log("Happy New Year");
+let i = 2018;
+while (i < 2025) {
   console.log(i);
-  i = i - 1;
+  i = i + 1;
 }
-console.log("Happy New Year");
