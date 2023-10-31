65263e74713a130849222e0718a94cb94b53d549
zain-student <zainmalik4989@gmail.com>
2023-10-31 11:33:10 +0500

    String CheatSheet

---
+++ stringCheatSheet.js
@@ -0,0 +1,11 @@
+var greet = "Hello";
+var name = "Zain";
+console.log(greet.charAt(0));
+console.log(greet.length);
+console.log(greet.concat(name));
+console.log(greet.indexOf("l"));
+console.log(greet.lastIndexOf("l"));
+console.log(greet.split("e"));
+console.log("Hello,World".split(","));
+console.log(greet.toUpperCase());
+console.log(greet.toLowerCase());
