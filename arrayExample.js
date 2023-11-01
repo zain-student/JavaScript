ec201236a24cf8e1d5af0c72d72df23d6b5f88cd
zain-student <zainmalik4989@gmail.com>
2023-11-01 11:15:42 +0500

    Array Practice

---
+++ arrayExample.js
@@ -0,0 +1,12 @@
+var clothes = [];
+clothes.push("TShirt", "Pent", "Dress Shirt", "Trouser", "Shalwar Kameez");
+console.log(clothes);
+clothes.pop();
+console.log(clothes);
+clothes.push("Costume");
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color = "Blue";
+favCar.convertible = true;
+console.log(favCar);
