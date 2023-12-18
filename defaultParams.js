0edd9ab484135e43fe1a84c61e81b6de3f3a4db5
Zain ul Abideen <zainmalik4989@gmail.com>
2023-12-18 08:34:45 +0500

    Default Parameters

---
+++ defaultParams.js
@@ -0,0 +1,36 @@
+// var num1, num2, num3, string1, bool1;
+// class NoDefaultParam {
+//   constructor(num1, num2, num3, string1, bool1) {
+//     this.num1 = num1;
+//     this.num2 = num2;
+//     this.num3 = num3;
+//     this.string1 = string1;
+//     this.bool1 = bool1;
+//   }
+//   calculate() {
+//     if (this.bool1) {
+//       console.log(string1, num1 + num2 + num3);
+//     }
+//     console.log("Not a true value.");
+//   }
+// }
+// var t1 = new NoDefaultParam();
+// t1.calculate();
+class WithDefaultParams {
+  constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.num3 = num3;
+    this.string1 = string1;
+    this.bool1 = bool1;
+  }
+  calculate() {
+    if (this.bool1) {
+      console.log(this.string1, this.num1 + this.num2 + this.num3);
+      return;
+    }
+    return "The value of bool1 is incorrect";
+  }
+}
+var better = new WithDefaultParams();
+better.calculate(); // Result: 6
